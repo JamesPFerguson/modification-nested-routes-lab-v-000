83715ea9f125984b1a02dd0645de64c98c3d83e5
James Ferguson <ncjamesferguson@gmail.com>
2018-04-14 17:48:35 +0000

    automatically backed up by learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,12 @@
 module SongsHelper
 
+
+  def artist_select(artist, song)
+    if song.artist.nil?
+      select_tag "song[artist_id]", collection_select(Artist.all, :id, :name)
+    else
+      hidden_field_tag "song[artist_id]", song.artist.id
+    end
+  end
+
 end
